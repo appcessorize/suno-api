957da3c694a59ba9296b108fc3332adce4921d76
appcessorize <rudecaptcha@outlook.com>
2024-06-12 19:52:09 +0900

    todo2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,22 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import TodoList from "@/components/TodoList";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col  ${inter.className} snap-y overflow-y-scroll snap-mandatory`}
+      className={`flex min-h-screen flex-col snap-y overflow-y-scroll snap-mandatory`}
     >
-      <div className="h-screen w-full bg-red-100 flex  items-center justify-center snap-start">
+      <div className="h-screen w-full bg-red-100 flex items-center justify-center snap-start">
         <div className="prose">
           <p>Yo man</p>
           <h2>tailwind test</h2>
+          <button onClick={() => console.log("clicked")}>Click me</button>
         </div>
       </div>
-      <div className="h-screen w-full bg-black flex  items-center  flex-col justify-center snap-start  bg-[url('https://c4.wallpaperflare.com/wallpaper/914/522/554/david-bowie-musician-smoking-wallpaper-preview.jpg')] bg-cover ">
-        <div className="bg-black/60 w-full h-full  flex items-center  flex-col justify-center ">
+      <div className="h-screen w-full bg-black flex items-center flex-col justify-center snap-start bg-[url('https://c4.wallpaperflare.com/wallpaper/914/522/554/david-bowie-musician-smoking-wallpaper-preview.jpg')] bg-cover">
+        <div className="bg-black/60 w-full h-full flex items-center flex-col justify-center">
           <p className="font-bold text-white px-4 md:px-10 w-full md:w-1/2 text-2xl md:text-5xl lg:text-7xl drop-shadow-lg text-center md:text-left">
             Music itself is going to become like running water or electricity
           </p>
@@ -27,22 +28,20 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="h-screen w-full bg-yellow-100  flex items-center justify-center snap-start ">
+      <div className="h-screen w-full bg-yellow-100 flex items-center justify-center snap-start">
         <div className="prose">
-          <p className="px-4 md:px-10 text-center ">
+          <p className="px-4 md:px-10 text-center">
             The absolute transformation of everything that we ever thought about
             music will take place within 10 years, and nothing is going to be
             able to stop it.
           </p>
-
-          <p className="px-4 md:px-10 text-center ">
+          <p className="px-4 md:px-10 text-center">
             I see absolutely no point in pretending that it’s not going to
             happen.
           </p>
         </div>
       </div>
-
-      <div className="h-screen w-full bg-black flex items-center  flex-col justify-center snap-start">
+      <div className="h-screen w-full bg-black flex items-center flex-col justify-center snap-start">
         <p className="font-bold text-white px-4 md:px-10 w-full md:w-1/2 text-2xl md:text-5xl lg:text-5xl drop-shadow-lg text-center md:text-left">
           What if your todo list could sing?
         </p>
@@ -50,10 +49,10 @@ export default function Home() {
           Would it help you to remember what you have to do?
         </p>
       </div>
-      <div className=" w-full bg-black flex  items-center justify-center snap-start">
+      <div className="w-full bg-black flex items-center justify-center snap-start">
         <TodoList />
       </div>
-      <div className="h-screen w-full bg-red-100 flex  items-center justify-center snap-start">
+      <div className="h-screen w-full bg-red-100 flex items-center justify-center snap-start">
         <p>hi</p>
       </div>
     </main>
